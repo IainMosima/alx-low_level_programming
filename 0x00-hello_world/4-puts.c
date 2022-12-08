f66dde3622835c1ba4d15d8ff2325d0df6704eaf
IainMosima <iainmosima@gmail.com>
2022-12-07 23:14:23 -0800

    Recitified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - prints out a string to stdout
+ * Returns 0 if successful
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
