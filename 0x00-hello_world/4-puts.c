71d4ec5962c844971117e5b4a2c1844074c8f491
IainMosima <iainmosima@gmail.com>
2022-12-07 23:16:16 -0800

    Recitified 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints out a string to stdout
- * Returns 0 if successful
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
