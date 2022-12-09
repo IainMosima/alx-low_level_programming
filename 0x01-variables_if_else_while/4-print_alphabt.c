f32254e50afad57f2722106011a3dcd81ca67398
IainMosima <iainmosima@gmail.com>
2022-12-09 18:14:11 +0300

    4-print_alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase followed by a new line
+ * Return: Always 0 is successful
+*/
+int main(void)
+{
+    char ch = 'a';
+    while(ch < 'z')
+    {
+        if ((ch != 'q') && (ch != 'e'))
+        {
+            putchar(ch);
+        }
+        ch++;
+    }
+    return (0);
+}
