b6f5e7b120ccaef5ef4a75d72d4c952fc7517372
IainMosima <iainmosima@gmail.com>
2022-12-10 10:46:39 +0300

    Recitified 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,11 +5,15 @@
 */
 int main(void)
 {
-int num = 0;
-while (num <= 9)
+int num = '0';
+while (num <= '9')
+{
+putchar(num);
+if (num != '9')
 {
 putchar(',');
 putchar(' ');
+}
 num++;
 }
 putchar('\n');
