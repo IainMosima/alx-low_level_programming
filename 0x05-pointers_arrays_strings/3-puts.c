5e3aaa6aee501afd671e4620735b3a6ac7e55593
IainMosima <iainmosima@gmail.com>
2022-12-19 20:27:00 +0300

    finished assignment

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _puts - function thta prints a string followed by a new line
- * @str: string parameter
-*/
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of a string
+ */
 void _puts(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
