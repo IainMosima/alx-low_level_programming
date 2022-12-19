d9d93704c95b80fea5ef01af41ee74b3adb73dcd
IainMosima <iainmosima@gmail.com>
2022-12-19 20:38:01 +0300

    correction

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _strlen - give the length of a string
  * @s: the string
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 /**
  * print_array - prints n times the elements of an array
