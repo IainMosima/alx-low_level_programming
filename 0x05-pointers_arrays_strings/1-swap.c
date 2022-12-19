cf36cd552f46aaae904b210dae2908f70a3702e7
IainMosima <iainmosima@gmail.com>
2022-12-19 20:00:18 +0300

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int: swaps the values of two integers
+ * @a: integer a
+ * @b: integer b
+*/
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 #ifdef MAIN_H
 #define MAIN_H
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
