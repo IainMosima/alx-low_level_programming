1e796b3a4272939c4beebfe44fcc228a9d3aecb6
IainMosima <iainmosima@gmail.com>
2023-01-09 17:51:27 +0300

    corrections

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: dest string
+ * @src: src string
+ * Retutn: pointer to resulting string
+*/
+char *_strcat(char *dest, char *src)
+{
+int s = 0, i = 0;
+while (dest[s] != 0)
+{
+s++;
+}
+while (src[i] != 0)
+{
+dest[s + i] = src[i];
+i++;
+}
+dest[s + i] = 0;
+return (dest);
+}
