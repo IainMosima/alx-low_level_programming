78d89f422dfd6800a6b05d23ceb7a21f2a2b7be3
IainMosima <iainmosima@gmail.com>
2023-01-10 12:33:07 +0300

    corrections

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,35 +8,34 @@
  *@s2: string 2
  * Return: pointer should point to a newly allocated space in memory or NULL
  */
-
 char *str_concat(char *s1, char *s2)
 {
-char *newStr = NULL;
-unsigned int i;
-int n1;
-int n2;
-int count;
+	char *strnew = NULL;
+	unsigned int i;
+	int n1;
+	int n2;
+	int count;
 
-count = 0;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-for (n1 = 0; s1[n1] != '\0'; n1++)
-;
-for (n2 = 0; s2[n2] != '\0'; n2++)
-;
-newStr = (char *)malloc((n1 + n2 + 1) * sizeof(char));
-if (newStr == NULL)
-{
-return (NULL);
-}
-for (i = 0; s1[i] != '\0'; i++)
-newStr[i] = s1[i];
-for (; s2[count] != '\0'; i++)
-{
-newStr[i] = s1[i];
-count++;
-}
-return (newStr);
+	count = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (n1 = 0; s1[n1] != '\0'; n1++)
+		;
+	for (n2 = 0; s2[n2] != '\0'; n2++)
+		;
+	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+	if (strnew == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		strnew[i] = s1[i];
+	for (; s2[count] != '\0'; i++)
+	{
+		strnew[i] = s2[count];
+		count++;
+	}
+	return (strnew);
 }
