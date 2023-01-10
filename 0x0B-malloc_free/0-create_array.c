fbb66d6c9a5420b082d6adec919277b6cc863c6c
IainMosima <iainmosima@gmail.com>
2023-01-10 12:05:13 +0300

    Corrections

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,34 +1,38 @@
-#include "main.h"
-#include "stdlib.h"
+#include "holberton.h"
+#include <stdlib.h>
 /**
- * create_array - creates an array of chats
- * @size: size of memory to print
+ * create_array - create an array of chars
+ * @size: size of the memory to print
  * @c: character to print
-*/
-char *create_array(unsigned int size, char *c)
+ *
+ * Return: a pointer of array or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
 {
-char *array;
-unsigned int i;
-i = 0;
-if (size == 0)
-{
-return ('\0');
-}
-else
-{
-array = malloc(sizeof(char) * size);
-if (array == '\0')
-{
-return ('\0');
-}
-else
-{
-while (i < size)
-{
-array[i] = c;
-i++;
-}
-}
-return (array);
-}
+	char *array;
+	unsigned int i;
+
+	i = 0;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	else
+	{
+		array = malloc(sizeof(char) * size);
+			if (array == '\0')
+			{
+				return ('\0');
+			}
+			else
+			{
+				while (i < size)
+				{
+					array[i] = c;
+					i++;
+				}
+			}
+		return (array);
+	}
 }
