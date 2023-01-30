2ab4ab9e684864489ef3df3a90b7f6c391d9a79d
IainMosima <iainmosima@gmail.com>
2023-01-30 11:33:13 +0300

    finished

--- 0x12-singly_linked_lists/0-print_listint.c
+++ 0x12-singly_linked_lists/0-print_listint.c
@@ -7,15 +7,16 @@
  *
  * Return: The number of nodes in the list_t list.
  */
-
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
-while(h)
-{
-nodes++;
-printf("%d\n", h->n);
-h = h->next;
-}
-return (nodes);
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
 }
